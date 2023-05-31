89744bb8d97d3861530d1f15328f2d6d633d84ef
xbmlz <997909544@qq.com>
2023-05-31 22:25:32 +0800

    feat: add icon demo

--- docs/components/icon.md
+++ docs/components/icon.md
@@ -9,8 +9,6 @@ Use any icon with Pure CSS for DivclsUI.
 
 For more icon information view [@unocss/preset-icons](https://unocss.dev/presets/icons)
 
-DivclsUI provides [carbon](https://icones.js.org/collection/carbon) icons by default.
-
 Get all icons from [here](https://icones.js.org/)
 
 ## Basic usage
--- docs/examples/icon/basic.vue
+++ docs/examples/icon/basic.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="space">
-    <div class="i-carbon-airplay"></div>
-    <div class="i-carbon:debug"></div>
+  <div class="space text-xl">
+    <div class="i-ri-bluetooth-line c-blue-6"></div>
+    <div class="i-ri-flag-2-line"></div>
+    <div class="i-ri-fingerprint-line c-orange-6"></div>
+    <div class="i-ri-alarm-fill"></div>
     <div class="i-ri-refresh-line animate-spin"></div>
   </div>
 </template>
